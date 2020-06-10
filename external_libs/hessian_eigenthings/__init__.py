5e25d50d7a1247ca29acf941117e1de7f8275d20
Iman Mirzadeh <iman.kinder@gmail.com>
2020-06-10 05:10:17 -0700

    Change hessian lib imports to relative

--- external_libs/hessian_eigenthings/__init__.py
+++ external_libs/hessian_eigenthings/__init__.py
@@ -1,9 +1,9 @@
 """ Top-level module for hessian eigenvec computation
 This library is cited in our paper.
 """
-from external_libs.hessian_eigenthings.power_iter import power_iteration, deflated_power_iteration
-from external_libs.hessian_eigenthings.lanczos import lanczos
-from external_libs.hessian_eigenthings.hvp_operator import HVPOperator, compute_hessian_eigenthings
+from .power_iter import power_iteration, deflated_power_iteration
+from .lanczos import lanczos
+from .hvp_operator import HVPOperator, compute_hessian_eigenthings
 
 __all__ = [
     "power_iteration",
