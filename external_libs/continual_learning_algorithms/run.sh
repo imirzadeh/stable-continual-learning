c0b86ab1defd2bb509e879557a062a91b1c431d9
Iman Mirzadeh <iman.kinder@gmail.com>
2020-06-10 04:54:02 -0700

    Update requirements

--- external_libs/continual_learning_algorithms/run.sh
+++ external_libs/continual_learning_algorithms/run.sh
@@ -1,3 +1,2 @@
-apt-get install libsm6 libxrender1 libfontconfig1 libsm6 libxext6 libxrender-dev
+apt-get install libsm6 libxrender1 libfontconfig1 libsm6 libxext6 libxrender-dev python-cairocffi python3-cairocffi libcurl4-openssl-dev libssl-dev
 python3 -m pip install opencv-contrib-python
-
