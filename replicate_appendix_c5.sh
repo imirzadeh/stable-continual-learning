cc8f8b73a61835ff3b5867b53bc6cd4f41a1bdc9
Iman Mirzadeh <iman.kinder@gmail.com>
2020-06-10 05:27:41 -0700

    Update log message

--- replicate_appendix_c5.sh
+++ replicate_appendix_c5.sh
@@ -1,3 +1,3 @@
-echo "Make sure you have tensorflow==1.15 installed."
+echo "Make sure you have tensorflow==1.15.2 installed."
 cd ./external_libs/continual_learning_algorithms
 bash replicate_mnist_stable.sh
