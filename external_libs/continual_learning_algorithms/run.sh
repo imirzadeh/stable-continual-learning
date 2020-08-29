994ce098cf417653c8c43cbe1ed99efdf904e009
Iman Mirzadeh <iman.kinder@gmail.com>
2020-08-29 06:03:33 +0000

    Add new required package for cv2

--- external_libs/continual_learning_algorithms/run.sh
+++ external_libs/continual_learning_algorithms/run.sh
@@ -1,2 +1,3 @@
 apt-get install libsm6 libxrender1 libfontconfig1 libsm6 libxext6 libxrender-dev python-cairocffi python3-cairocffi libcurl4-openssl-dev libssl-dev
+apt-get install libgl1-mesa-glx
 python3 -m pip install opencv-contrib-python
